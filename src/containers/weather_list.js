c820791a84422ecb62908eb068f97e283d270a02
youngfreezy <Fareez.Ahmed@gmail.com>
2016-07-11 16:18:08 +0600

    sparkline charts progress

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
 class WeatherList extends Component {
+  renderWeather(cityData) {
+  const name = cityData.city.name;
+  const temps = cityData.list.map(weather => weather.main.temp);
+  const pressures = cityData.list.map(weather => weather.main.pressure);
+  const humidities = cityData.list.map(weather => weather.main.humidity);
+
+  return (
+    <tr key={name}>
+      <td>{name}</td>
+      <td><Chart data={temps} color="orange" units="K"/></td>
+      <td><Chart data={pressures} color="green" units="hPa"/></td>
+      <td><Chart data={humidities} color="black" units="K"/></td>
+    </tr>
+  )
+  } 
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity(%)</th>
           </tr>
         </thead>
         <tbody>
@@ -19,20 +34,6 @@ class WeatherList extends Component {
       </table>
     )
   }
-
-  renderWeather(cityData) {
-  const name = cityData.city.name;
-  const temps = cityData.list.map(weather => weather.main.temp);
-  console.log(temps);
-  return (
-    <tr key={name}>
-      <td>{name}</td>
-      <td>
-        <Chart data={temps} color="orange"/>        
-      </td>
-    </tr>
-  )
-} 
 
 }
 
