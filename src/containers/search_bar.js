f4d3d1975dfb51220516a7545e5dea20d211258e
youngfreezy <Fareez.Ahmed@gmail.com>
2016-07-11 12:46:07 +0600

    undefined this in event handler

---
+++ src/containers/search_bar.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+export default class SearchBar extends Component {
+  //initialize state first
+  constructor(props) {
+    super(props);
+    this.state = {term : ''};
+  }
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Get a five day forecast"
+          className="form-control"
+          //make it a controlled componenet by input value to state
+          value={this.state.term}
+          //this undefined b/c wrapped it in fat arrow function before, which synced scopes
+          onChange={this.onInputChange}/>
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+  }
+}
