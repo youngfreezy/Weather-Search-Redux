f400b969bcc87b9f7273fa1de680f949a4dbb56b
youngfreezy <Fareez.Ahmed@gmail.com>
2016-07-11 12:48:07 +0600

    fix for undefined this with bind in constructor

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,6 +4,7 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {term : ''};
+    this.onInputChange = this.onInputChange.bind(this);
   }
   render() {
     return (
