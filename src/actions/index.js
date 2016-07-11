1594195481d705ed3a12206225b1f9d555d094f7
youngfreezy <Fareez.Ahmed@gmail.com>
2016-07-11 13:49:06 +0600

    wire up axios for ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
+import axios form 'axios';
 const API_KEY='bbf7e72ff9a84399da3ee3205ad911c9';
-// sample request:
-// http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=bbf7e72ff9a84399da3ee3205ad911c9
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export function fetchWeather (city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
